bd8b0fe71a35d83ef1ef9934e59eac9fbab1867d
alilighto <alilighto02@gmail.com>
2023-10-10 12:08:00 +0100

    Add script to change ownership of a file

--- learning.sh
+++ learning.sh
@@ -115,3 +115,4 @@
 # Add script to set multiple permissions at once
 # Add script using parameter expansion ${VAR:-default}
 # Add interactive script to ask for input
+# Add script to change ownership of a file
