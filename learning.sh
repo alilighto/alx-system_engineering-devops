39da697c8987038df8f70278569e3104c57bf622
alilighto <alilighto02@gmail.com>
2023-11-11 12:00:00 +0100

    Add script that evaluates expressions with `expr`

--- learning.sh
+++ learning.sh
@@ -438,3 +438,4 @@
 # Add script redirecting stderr to stdout
 # Fix invalid characters in variable names
 # Add script showing use of `env` command
+# Add script that evaluates expressions with `expr`
